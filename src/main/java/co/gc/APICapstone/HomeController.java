5d001192348a4c9345d77507bd2c06659eca8f6e
kbaskin <kathrynbaskin@gmail.com>
2019-12-04 13:37:13 -0500

    updated controller

--- src/main/java/co/gc/APICapstone/HomeController.java
+++ src/main/java/co/gc/APICapstone/HomeController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.ModelAndView;
 
-import co.gc.APICapstone.entity.EventResults;
+import co.gc.APICapstone.entity.Event;
 
 @Controller
 public class HomeController {
@@ -22,13 +22,13 @@ public class HomeController {
 //		headers.add(HttpHeaders.USER_AGENT, "Testing!");
 //		headers.add("Accept", MediaType.APPLICATION_JSON_VALUE);
 //		HttpEntity<String> entity = new HttpEntity<>("parameters", headers); 
-		String url = "https://app.ticketmaster.com/discovery/v2/events?apikey=os6UnKvE5d7uqNVEhHKDGL9ReGhTHcRB&locale=*";
-		EventResults eventResults = rt.getForObject(url, EventResults.class);
+		Event response = rt.getForObject("https://app.ticketmaster.com/discovery/v2/events.json?size=1&apikey=" + eventKey, Event.class);
+
 		
 //		String response = rt.getForObject("https://app.ticketmaster.com/discovery/v2/events.json?size=1&apikey=" + eventKey, String.class);
 //		System.out.println(response);
 
-		return new ModelAndView("index", "eventResults", eventResults);
+		return new ModelAndView("index", "eventResults", response);
 	}
 	
 	
