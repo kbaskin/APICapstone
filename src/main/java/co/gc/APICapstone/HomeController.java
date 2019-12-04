5fb67179857ec84f77e8542520c514507f8d93de
Tmogk <tylercmogk@gmail.com>
2019-12-04 10:38:47 -0500

    API String return test

---
+++ src/main/java/co/gc/APICapstone/HomeController.java
@@ -0,0 +1,28 @@
+package co.gc.APICapstone;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HomeController {
+	
+	RestTemplate rt = new RestTemplate();
+	
+	@Value("${events.key}")
+	private String eventKey;
+	
+	@RequestMapping("/")
+	public ModelAndView home() {
+		
+		
+		String response = rt.getForObject("https://app.ticketmaster.com/discovery/v2/events.json?size=1&apikey=" + eventKey, String.class);
+		System.out.println(response);
+		
+		
+		return new ModelAndView();
+	}
+
+}
